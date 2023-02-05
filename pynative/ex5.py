17fe93b2ba5def8e03ffd9d9ffd83e2dde2a27f0
thicchead <metehan.karakoruk@gmail.com>
2023-02-05 16:59:46 +0100

    exercise 5 OK

--- pynative/ex5.py
+++ pynative/ex5.py
@@ -1,4 +1,28 @@
+def check_numbers(lst):
+    first_number = lst[0]
+    last_number = lst[len(lst) - 1]
+    # Can put -1 as the index
+
+    if first_number == last_number:
+        return True
+    else:
+        return False
+
+
 def main():
+    number_list = []
+
+    list_size = int(input("Size of list: "))
+
+    for i in range(list_size):
+        num = int(input("Number: "))
+
+        number_list.append(num)
+
+    print("Given list: " + str(number_list))
+    print("Result is " + str(check_numbers(number_list)))
+
+# WORKS, but with input, not hardcoded. (looked up how to put input into a list https://www.geeksforgeeks.org/python-get-a-list-as-input-from-user/)
 
 
 if __name__ == '__main__':
